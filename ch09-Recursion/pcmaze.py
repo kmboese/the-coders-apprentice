381028e07b28912af933eb743c66d70e69032d67
Kevin Boese <kmboese@ucdavis.edu>
2018-07-28 09:51:53 -0700

    Implemented DFS maze solution

--- ch09-Recursion/pcmaze.py
+++ ch09-Recursion/pcmaze.py
@@ -1,3 +1,12 @@
+# Range of cells in the maze
+LOWEST_CELL = 1
+HIGHEST_CELL = 16
+
+# Global variables
+cellList = []
+visitedList = []
+exitFound = False
+
 def connected( x, y ):
     if x > y:
         return connected( y, x )
@@ -6,9 +15,48 @@ def connected( x, y ):
         (15,16)):
         return True
     return False
+
+def edges(x):
+    return ([i for i in range(LOWEST_CELL, HIGHEST_CELL+1) if connected(x,i)])
     
 def entrance():
-    return 1
+    return LOWEST_CELL
     
 def exit():
-    return 16
+    return HIGHEST_CELL
+
+def findPathToExit(current=entrance()):
+    global cellList
+    global visitedList
+    global exitFound
+
+    if current == entrance():
+        visitedList.append(current)
+
+    # Base case: we found the exit
+    if (current == exit()):
+        exitFound = True
+        return current
+    
+    # DFS path finding
+    for cell in edges(current):
+        if cell not in visitedList:
+            visitedList.append(cell)
+            findPathToExit(cell)
+            if exitFound and connected(current,cell):
+                cellList.append(cell)
+                # Special case: append the entrance on the last recursive step
+                if (connected(cell, entrance())):
+                    cellList.append(entrance())
+                return cell
+
+
+def main():
+    global cellList
+    global visitedList
+    findPathToExit()
+    print("Finding path...\nPath to exit is {}".format(cellList))
+    print("Cells visited: {}".format(visitedList))
+
+if __name__ == "__main__":
+    main()
