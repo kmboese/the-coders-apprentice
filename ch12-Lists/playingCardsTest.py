333016752f13bad042f45c259ef2f4dabb100864
Kevin Boese <kmboese@ucdavis.edu>
2018-07-31 20:03:26 -0700

    Added test case to ensure shuffle is working correctly

--- ch12-Lists/playingCardsTest.py
+++ ch12-Lists/playingCardsTest.py
@@ -1,9 +1,21 @@
 from playingCards import *
 
+def testShuffle():
+    original = createDeck()
+    shuffled = shuffleDeck(original)
+    
+    assert(len(original) == len(shuffled))
+    for card in shuffled:
+        assert(card in original)
+
+
 def main():
     deck = createDeck()
     print("Original deck: {}".format(deck))
     shuffledDeck = shuffleDeck(deck)
     print("Shuffled deck: {}".format(shuffledDeck))
+
+    # Verify that the shuffled deck contains the same cards as the original deck
+    testShuffle()
 if __name__ == "__main__":
     main()
